14d0ada150e60fb6875fd932ab7cf57cca2d3e8a
Jarrell Jackson <jdjack3@gmail.com>
2020-08-20 20:06:33 +0000

    Automatically backed up by Learn

---
+++ lib/doggy.rb
@@ -0,0 +1,15 @@
+class Dog
+
+  def name=(dogs_name)
+    @this_dog_name = dogs_name
+  end
+
+  def name
+    @this_dog_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
